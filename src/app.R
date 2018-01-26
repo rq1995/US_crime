f25336783c791fcf984b97f43250f35c786267cc
RUOQI XU <7Ymr@AppledeAir-3.hitronhub.home>
2018-01-26 13:08:25 -0800

    Add second plot and plotly

--- src/app.R
+++ src/app.R
@@ -5,6 +5,7 @@ library(lubridate)
 library(htmltools)
 library(RColorBrewer)
 library(colourpicker)
+library(plotly)
 
 # import previously cleaned data
 crime <- read_csv("https://raw.githubusercontent.com/rq1995/US_crime/master/data/ucr_crime_1975_2015.csv")
@@ -19,27 +20,34 @@ year<-unique(crime$year)
 ui <- fluidPage(
   
   # Application title
-  titlePanel("Crime data for US APP"),
+  titlePanel("Crime Data Analysis in US"),
   
-  # Input
+  # Input and layout
   sidebarLayout(
     sidebarPanel(
-      selectizeInput('regioninput','Region',choices=department,multiple=TRUE,selected=department_short),
+      p("This app can help people to discover the best city from crime rate and population aspects, before they invest, travel, or select a location to live."),
+      br(),
+      selectizeInput('regioninput','Region (multiple selections allowed)',choices=department,multiple=TRUE,selected=department_short),
+      p('Add new region from dropdown box and strightly delete'),
+      br(),
       sliderInput('Yearinput','Year',min=1975,max = 2015,step = 1,value = c(1975,2015)),
-      radioButtons('crimetypeinput', 'Select type',
-                 choices = c("violent_crime", "homs_sum", "rape_sum", "rob_sum","agg_ass_sum"),
-                 selected = "violent_crime"),
-      colourInput("colourInput1","colour",value="black")
+      br(),
+      radioButtons('crimetypeinput', 'Select crime type',
+                 choices = c("Total Violent"="violent_crime", 
+                             "Homicide"="homs_sum", 
+                             "Rape"="rape_sum", 
+                             "Robbery"="rob_sum",
+                             "Aggravated Assault"="agg_ass_sum"),
+                 selected = "violent_crime")
   ),
     
     # Show a plot of the generated distribution
     mainPanel(
-      h4(textOutput('text')),
-      plotOutput('densityplot'),
+      h4("Crime Rate of selecte region"),
+      plotlyOutput('densityplot',width="100%",height=300),
       br(),
-      ggvisOutput("plot1"),
-      br(),
-      tableOutput('result')
+      h4("The trend of population in selecte region"),
+      plotlyOutput('barplot',width="100%",height=300)
     )
   )
 )
@@ -47,45 +55,41 @@ ui <- fluidPage(
 # Define server 
 server <- function(input, output,session) {
   
-  output$text<-renderText({
-    filtered <-
-      crime %>%
-      filter(department_name== input$regioninput,
-             year >= as.numeric(input$Yearinput[1]) & year <= as.numeric(input$Yearinput[2])
-      )
-    paste0("There are ",input$crimetypeinput, " crime record")
-  })
-  output$densityplot <- renderPlot({
+  output$densityplot <- renderPlotly({
     filtered <-
       crime %>%
       filter(department_name %in% input$regioninput,
              year >= as.numeric(input$Yearinput[1]) & year <= as.numeric(input$Yearinput[2]))
     if (input$crimetypeinput=="violent_crime") {
       ggplot(filtered, aes(year, violent_per_100k,colour=department_name)) +
-        geom_line()+geom_point()+guides(colour = FALSE)
-       # geom_path(colour=input$colourInput1)
+        geom_line()+
+        geom_point(aes(alpha=0.2))+guides(colour = FALSE)
     }else if(input$crimetypeinput=="homs_sum") {
       ggplot(filtered, aes(year,homs_per_100k,colour=department_name)) +
-        geom_line()+geom_point()+guides(colour = FALSE)
+        geom_line()+geom_point(aes(alpha=0.2))+guides(colour = FALSE,alpha=FALSE)
     }else if (input$crimetypeinput=="rape_sum") {
       ggplot(filtered, aes(year,rape_per_100k,colour=department_name)) +
-        geom_line()+geom_point()+guides(colour = FALSE)
+        geom_line()+geom_point(aes(alpha=0.2))+guides(colour = FALSE)
     }else if (input$crimetypeinput=="rob_sum") {
       ggplot(filtered, aes(year,rob_per_100k,colour=department_name)) +
-        geom_line()+geom_point()+guides(colour = FALSE)
+        geom_line()+geom_point(aes(alpha=0.2))+guides(colour = FALSE)
     }else if (input$crimetypeinput=="agg_ass_sum") {
       ggplot(filtered, aes(year,agg_ass_per_100k,colour=department_name)) +
-        geom_line()+geom_point()+guides(colour = FALSE)
+        geom_line()+geom_point(aes(alpha=0.2))+guides(colour = FALSE)
     }
 })
-  output$result<-renderTable({
+
+  
+  output$barplot <- renderPlotly({
+    
     filtered <-
       crime %>%
       filter(department_name %in% input$regioninput,
-             year >= as.numeric(input$Yearinput[1]) & year <= as.numeric(input$Yearinput[2]))%>%
-      group_by(department_name)%>%
-      summarise(mean=mean(total_pop))%>%
-      select(department_name,mean)
+             year >= as.numeric(input$Yearinput[1]) & year <= as.numeric(input$Yearinput[2]))
+      ggplot(filtered,aes(x = year, y =total_pop)) +
+      geom_line()+
+      facet_wrap(~ department_name)+
+      theme_bw() 
   })
 }
 
